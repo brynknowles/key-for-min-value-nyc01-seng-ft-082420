8560b2538fd5251e376838a0f61334dc5b3df545
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-19 16:48:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_value = nil
+  smallest_key = nil
   name_hash.each do |key, value|
     if smallest_value == nil
       smallest_key = key
@@ -16,4 +17,15 @@ def key_for_min_value(name_hash)
   smallest_key
 end
 
-
+# lowest_number = nil
+# name_hash.each do |name,number|
+# if lowest_number == nil
+#   answer = name
+#   lowest_number = number
+# end
+# if lowest_number > number
+#   answer = name
+#   lowest_number = number
+# end
+# end
+# answer
