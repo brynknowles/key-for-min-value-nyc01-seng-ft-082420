43c37139e54e9fc44eaae9e4965d4ccd561318c2
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-19 16:04:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest_value = {}
+  
   name_hash.each do |key, value|
+    if name_hash[key][value] <  
   end
 end
